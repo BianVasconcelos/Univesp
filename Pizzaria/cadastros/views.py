580255774bb2e783d932c548313d261038fcf1ba
fabianopc <fabianop@gmail.com>
2023-05-27 14:20:24 -0300

    Desenvolvendo o login backend

--- Pizzaria/cadastros/views.py
+++ Pizzaria/cadastros/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
@@ -7,6 +11,75 @@ from .forms import PizzaForm
 
 from django.urls import reverse_lazy
 
+#funcao de login para autendicacao
+def login_usuario(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('cadastrar_pedido')
+        else:
+            return render(request, 'login.html', {'mensagem': 'Crendenciais invalidas'})
+    else:
+        return render(request, 'login.html')
+    
+@login_required
+def cadastrar_pedido(request):
+    if request.method == 'POST':
+        #Obter os dados do formulario
+        pizzas = request.POST.getlist('pizza')
+        quantidades = request.POST.getlist('quantidade')
+        tamanho = request.POST.get('tamanho')
+        tipo_pagamento = request.POST.get('tipo_pagamento')
+        
+        #Obter usuario autenticado
+        user = request.user
+        
+        #Obter a pessoa relacionada ao usuario autenticado
+        pessoa = user.pessoa
+        
+        #Criar o objeto pedido
+        pedido = Pedido.objects.create(
+            valor_total = 0, #Sera atualizado posteriormente
+            tipo_pagamento = tipo_pagamento,
+            pedido = pessoa,
+            login = user.login
+        )
+        
+        #Cadastrar os itend do pedido
+        valor_total = 0
+        for pizza_id, quantidade in zip(pizzas, quantidades):
+            pizza = Pizza.objects.get(id=pizza_id)
+            preco_unitario = get_preco_por_tamanho(pizza, tamanho)
+            valor_item = preco_unitario * int(quantidade)
+            
+            #Criar o objeto ItensPedido
+            item = ItensPedido.objects.create(
+                item_id = pizza_id,
+                tipo_item = 'Pizza',
+                quantidade = int(quantidade),
+                preco_unitario = preco_unitario,
+                pizza = pizza,
+                pedido = pedido                
+            )
+            
+            valor_total += valor_item
+            
+        #Atualizar o valor total do pedido
+        pedido.valor_total = valor_total
+        pedido.save()
+        
+        return redirect('pagina_de_sucesso')
+        
+    else:
+        #Renderizar o template de cadastro de pedido
+        return render(request, 'cadastrar_pedido.html')    
+            
+
+####################### CREATE ####################### 
 # Create your views here.
 class PessoaCreate(CreateView):
     model = Pessoa
